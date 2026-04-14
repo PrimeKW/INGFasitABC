dc35d3dc2b59532d4273b7f46b5262c656eb945a
PrimeKW <kwsaatvedt@hotmail.com>
2026-04-14 14:46:01 +0200

    Riktig Path

--- tests/test_part_a.py
+++ tests/test_part_a.py
@@ -2,10 +2,8 @@ from unittest import TestCase, main
 
 from pathlib import Path
 import sys 
-sys.path.append(str(Path().parent.absolute()))
-
 from smarthouse.domain import SmartHouse
-from demo_house import DEMO_HOUSE as h
+from tests.demo_house import DEMO_HOUSE as h
 
 class TestPartA(TestCase):
 
